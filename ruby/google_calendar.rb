8bc7c8a2f9541f38e0ccef91260798610c5a5162
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2014-05-27 19:47:43 +0900

    add some calendar api sample

--- ruby/google_calendar.rb
+++ ruby/google_calendar.rb
@@ -1,5 +1,6 @@
 require 'google/api_client'
 require 'google/api_client/client_secrets'
+require 'json'
 
 client = Google::APIClient.new(
   application_name: 'pekobot',
@@ -13,8 +14,9 @@ client.authorization = client_secrets.to_authorization
 client.authorization.scope = 'https://www.googleapis.com/auth/calendar'
 client.authorization.fetch_access_token!
 
-year = 2013
-month = 7
+year = 2014
+month = 5
+day = 28
 
 time_min = Time.utc(year, month, 1, 0).iso8601
 time_max = Time.utc(year, month, 31, 0).iso8601
@@ -24,6 +26,33 @@ params = {'calendarId' => 'primary',
           'timeMax' => time_max,
           'timeMin' => time_min,
           'singleEvents' => 'True'}
+
+my_calendar = client.execute(
+  api_method: calendar.calendars.get,
+  parameters: {
+    calendarId: 'ryopeko@gmail.com'
+  }
+).data
+
+p client.execute(
+  api_method: calendar.events.insert,
+  parameters: {
+    calendarId: my_calendar.id
+  },
+  headers: {
+    'Content-Type' => 'application/json'
+  },
+  body: {
+    email: 'ryopeko@gmail.com',
+    start: {
+      dateTime: Time.utc(year, month, day, 18).iso8601.tap{|s| p s}
+    },
+    end: {
+      dateTime: Time.utc(year, month, day, 19).iso8601
+    },
+    summary: 'test event'
+  }.to_json
+).data
 
 p client.execute(
   api_method: calendar.events.list,
