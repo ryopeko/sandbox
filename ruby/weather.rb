e8cc5e84e0687ea845643f82ef3be778e6c14156
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2014-06-03 23:44:17 +0900

    body

--- ruby/weather.rb
+++ ruby/weather.rb
@@ -1,4 +1,4 @@
 require 'faraday'
 require 'json'
 
-p JSON.parse((Faraday.get 'http://weather.livedoor.com/forecast/webservice/json/v1?city=400040').body)
+p JSON.parse((Faraday.get 'http://weather.livedoor.com/forecast/webservice/json/v1?city=130010').body)['forecasts']
