b2fafeee629e19eeed6050d3337891879eefa589
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2014-06-24 20:37:39 +0900

    add unique index

--- rails/authentication_test/db/schema.rb
+++ rails/authentication_test/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140622054830) do
+ActiveRecord::Schema.define(version: 20140624113628) do
 
   create_table "users", force: true do |t|
     t.string   "name",            null: false
@@ -20,5 +20,7 @@ ActiveRecord::Schema.define(version: 20140622054830) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
