32db7cfeccadcf71a1eb5ea1d7a1638659cb4e80
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2014-06-25 21:39:02 +0900

    add before filter

--- rails/authentication_test/app/controllers/application_controller.rb
+++ rails/authentication_test/app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
+
+  def authenticate
+    redirect_to sessions_new_path unless signed_in?
+  end
 end
--- rails/authentication_test/app/controllers/users_controller.rb
+++ rails/authentication_test/app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, only: [:show]
   def new
     @user = User.new
   end
