66c7153c952d263647799e8fd5bb2d5fd565fba9
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2014-09-08 23:37:32 +0900

    Add grape test

---
+++ rails/grape_test/app/apis/books/api.rb
@@ -0,0 +1,18 @@
+module Books
+  class API < Grape::API
+    format :json
+    version 'v1'
+
+    resource :books do
+      desc 'get'
+      params do
+        requires :id, type: Integer, desc: 'Book ID'
+      end
+      route_param :id do
+        get do
+          Book.find(params[:id])
+        end
+      end
+    end
+  end
+end
