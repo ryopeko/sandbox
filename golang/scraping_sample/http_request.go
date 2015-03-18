7ef253edfba398b489d5ddf7878d55564e820ecb
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2015-03-19 02:55:10 +0900

    slice test

--- golang/scraping_sample/http_request.go
+++ golang/scraping_sample/http_request.go
@@ -8,8 +8,12 @@ import (
 func main() {
 	response, _ := goquery.NewDocument("http://b.hatena.ne.jp/entrylist")
 
+	urls := []string{}
+
 	response.Find("a").Each(func(_ int, s *goquery.Selection) {
 		url, _ := s.Attr("href")
-		fmt.Println(url)
+
+		urls = append(urls, url)
 	})
+	fmt.Println(urls)
 }
