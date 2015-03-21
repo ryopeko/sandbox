730cc16d90e7baf01e48a478bfd15e03fd4a7a2a
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2015-03-21 23:01:29 +0900

    int, float

--- golang/reflect_sample.go
+++ golang/reflect_sample.go
@@ -7,6 +7,8 @@ import (
 
 func main() {
 	slice := []string{}
+	fmt.Println(reflect.TypeOf(123))
+	fmt.Println(reflect.TypeOf(123.45))
 	fmt.Println(reflect.TypeOf("foo"))
 	fmt.Println(reflect.TypeOf(slice))
 }
