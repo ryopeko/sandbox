03f5f9c455028ac54e101a913a435759ea53a707
ryopeko <ryopeko+free@gmail.com>
2015-03-29 21:18:00 +0900

    sync goroutine

--- golang/goroutine_sample.go
+++ golang/goroutine_sample.go
@@ -3,13 +3,18 @@ package main
 import (
 	"fmt"
 	"time"
+	"sync"
 )
 
 func main() {
+	wg := new(sync.WaitGroup)
+
+	wg.Add(1)
 	go func() {
 		time.Sleep(3)
 		fmt.Println("Hello")
+		wg.Done()
 	}()
 	fmt.Println("Hi")
-
+	wg.Wait()
 }
