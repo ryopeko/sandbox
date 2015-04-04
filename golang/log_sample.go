e16c75d0bd277c846273e3e744ad02ffc3abf748
ryopeko <ryopeko+free@gmail.com>
2015-04-04 23:29:47 +0900

    set output file

--- golang/log_sample.go
+++ golang/log_sample.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"log"
+	"os"
 )
 
 func main() {
-	log.Panicln("bar")
+	log_file, _ := os.OpenFile("/tmp/test.log", os.O_RDWR|os.O_CREATE, 0644)
+
+	log.SetOutput(log_file)
+	log.Println("bar")
 }
