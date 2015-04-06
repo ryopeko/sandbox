7dd0150f15f15ba7c25634be6e3cfcdc831e117f
ryopeko <ryopeko+free@gmail.com>
2015-04-06 17:31:35 +0900

    file share test

--- golang/log_sample.go
+++ golang/log_sample.go
@@ -8,7 +8,9 @@ import (
 func main() {
 	log_file, _ := os.OpenFile("/tmp/test.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 
-	new_logger := log.New(log_file, "prefix:", log.Ldate|log.Ltime|log.Lshortfile)
+	new_logger := log.New(log_file, "logger1:", log.Ldate|log.Ltime|log.Lshortfile)
+	other_logger := log.New(log_file, "logger2:", log.Ldate|log.Ltime|log.Lshortfile)
 
-	new_logger.Println("bar")
+	new_logger.Println("foo")
+	other_logger.Println("bar")
 }
