7d53db3a3b59b5192543bd528e45283c28f5bfed
ryopeko <ryopeko+free@gmail.com>
2015-04-08 18:57:35 +0900

    create client

--- golang/twitter_sample.go
+++ golang/twitter_sample.go
@@ -3,9 +3,21 @@ package main
 
 import (
 	"fmt"
+	"github.com/kurrik/oauth1a"
 	"github.com/kurrik/twittergo"
+	"io/ioutil"
+	"strings"
 )
 
 func main() {
+	config_file, _ := ioutil.ReadFile("CREDENTIALS")
+	lines := strings.Split(string(config_file), "\n")
+
+	config := &oauth1a.ClientConfig{
+		ConsumerKey: lines[0],
+		ConsumerSecret: lines[1]
+	}
+	user := &oauth1a.NewAuthorizedConfig(lines[2], lines[3])
+	client := twittergo.NewClient(config, user)
 
 }
